84392929f04cbe0cf7642bc2cf168b061eeef3f9
germancardenasm <germancardenas@gmail.com>
2019-06-16 10:57:36 -0500

    se va a agregar el modal en la sedcion Works

--- JS/script.js
+++ JS/script.js
@@ -4,24 +4,31 @@ let resume = document.getElementById("resume");
 let getResume = document.getElementById("getResume");
 let works = document.getElementById("works");
 let getWorks = document.getElementById("getWorks");
-let blog = document.getElementById("blog");
-let getBlog = document.getElementById("getBlog");
+let worksCategories = document.getElementById('worksCategories');
+let worksContent = document.getElementById('works-content');
 let contact = document.getElementById("contact");
 let contact_me = document.getElementById("contact_me");
 let getContact = document.getElementById("getContact");
 let send = document.getElementById("send");
-var email = document.getElementById("email");
-var form = document.getElementById("form");
+let email = document.getElementById("email");
+let form = document.getElementById("form");
 
-function remove() {
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+function removeSection() {
   about.classList.remove("view");
   getAbout.classList.remove("selected");
   resume.classList.remove("view");
   getResume.classList.remove("selected");
   works.classList.remove("view");
   getWorks.classList.remove("selected");
-  blog.classList.remove("view");
-  getBlog.classList.remove("selected");
   contact.classList.remove("view");
   getContact.classList.remove("selected");
 }
@@ -35,9 +42,6 @@ getResume.addEventListener("click", function(e) {
 getWorks.addEventListener("click", function(e) {
   getSection(e, works, getWorks);
 });
-getBlog.addEventListener("click", function(e) {
-  getSection(e, blog, getBlog);
-});
 getContact.addEventListener("click", function(e) {
   getSection(e, contact, getContact);
 });
@@ -48,11 +52,41 @@ contact_me.addEventListener("click", function(e) {
 function getSection(event, section, getButton) {
   if (window.innerWidth > 1040) {
     event.preventDefault();
-    remove();
+    removeSection();
     section.classList.add("view");
     getButton.classList.add("selected");
   }
 }
+
+
+worksCategories.addEventListener('click', function(e){
+
+  let worksContainers = document.getElementsByClassName('work-container');
+  
+  for(let i = 0; i < worksContainers.length; i++) { 
+
+    if(worksCategories.children[i].innerHTML.toLowerCase() == e.target.innerHTML.toLowerCase())
+      worksCategories.children[i].classList.add('selected');
+    else 
+      worksCategories.children[i].classList.remove('selected');
+    
+    
+    if(e.target.innerHTML.toLowerCase()==='all')
+      worksContainers[i].classList.remove('hidden');
+    else
+
+      if(!worksContainers[i].classList.contains(e.target.innerHTML.toLowerCase()))
+        worksContainers[i].classList.add('hidden');
+       else
+        worksContainers[i].classList.remove('hidden');
+  }
+ 
+})
+
+worksContent.addEventListener('click', function(e){
+  modal.style.display = "block";
+
+});
 
 email.addEventListener("input", function(event) {
   if (email.validity.typeMismatch) {
@@ -78,3 +112,22 @@ form.addEventListener("submit", function(event) {
   Te respondere cuanto antes.`)
 
 });
+
+
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
